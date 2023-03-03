8d02d791f69987fdb980d1e7563638f8906a912d
Dat Doan <42245488+SmileDragonfly@users.noreply.github.com>
2023-03-03 22:37:14 +0700

    + Use local DB + Add log file

--- CryptoTrackingAgent/Utils.go
+++ CryptoTrackingAgent/Utils.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"CryptoTrackingSql/sqlc"
+	"context"
+	"database/sql"
 	"fmt"
 	"runtime"
+	"time"
 )
 
 func getCurrentFuncname() string {
 	pc, _, _, _ := runtime.Caller(1)
-	return fmt.Sprintf("%s", runtime.FuncForPC(pc).Name())
+	return fmt.Sprintf("%s-%s", time.Now().String(), runtime.FuncForPC(pc).Name())
+}
+
+func insertTblBUSDPrice(strPrice string, strDBConn string, strDBDriver string) error {
+	conn, err := sql.Open(strDBDriver, strDBConn)
+	if err != nil {
+		return err
+	}
+	query := sqlc.New(conn)
+	err = query.InsertBUSDPrice(context.Background(), sql.NullString{strPrice, true})
+	return err
 }
--- CryptoTrackingAgent/main.go
+++ CryptoTrackingAgent/main.go
@@ -1,27 +1,32 @@
 package main
 
 import (
-	"CryptoTrackingSql/sqlc"
-	"context"
-	"database/sql"
 	"fmt"
 	_ "github.com/lib/pq"
+	"log"
+	"os"
 )
 
+var GStrConn string
+
 func main() {
+	// Init log file
+	f, err := os.OpenFile("CryptoTrackingAgent.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	defer f.Close()
+	log.SetOutput(f)
+	log.Println("Start CryptoTrackingAgent")
+	// Begin main program
 	var api BinanceAPI
 	loadConfig(".")
 	strPrice := api.getAllPrice("BUSD$")
 	// Insert to DB
-	strConn := fmt.Sprintf("port=%d host=%s user=%s password=%s dbname=%s sslmode=disable",
+	GStrConn = fmt.Sprintf("port=%d host=%s user=%s password=%s dbname=%s sslmode=disable",
 		GConfig.HostPort, GConfig.HostName, GConfig.UserName, GConfig.Password, GConfig.DBName)
-	conn, err := sql.Open(GConfig.DBDriver, strConn)
+	err = insertTblBUSDPrice(strPrice, GStrConn, GConfig.DBDriver)
 	if err != nil {
-		fmt.Print(getCurrentFuncname(), err.Error())
-	}
-	query := sqlc.New(conn)
-	err = query.InsertBUSDPrice(context.Background(), sql.NullString{strPrice, true})
-	if err != nil {
-		fmt.Print(getCurrentFuncname(), err.Error())
+		log.Println(err.Error())
 	}
 }
