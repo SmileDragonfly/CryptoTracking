e4df136c36452840d04c1020d3b0fa3e00ddff2f
Dat Doan <42245488+SmileDragonfly@users.noreply.github.com>
2023-05-29 17:48:32 +0700

    + Load data immediately and reload after timeout

--- crypto-tracking-web-react/src/components/ReactTable.js
+++ crypto-tracking-web-react/src/components/ReactTable.js
@@ -6,30 +6,31 @@ function ReactTable({endPoint, caption}){// Define state variables for JSON data
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(0);
 
-// Define an async function to fetch data from API
-    async function fetchData(endPoint) {
-        try {
-            let headers = new Headers()
-            headers.append('ngrok-skip-browser-warning', true)
-            // headers.append('Access-Control-Allow-Origin', '*')
-// Make a GET request and get the response
-            const response = await fetch(endPoint, {headers: headers});
-// Convert the response to JSON format
-            const json = await response.json();
-// Update the data state with the JSON data
-            setData(json);
-// Set the loading state to false
-            setLoading((c)=> c+1);
-        } catch (error) {
-// Handle any errors
-            console.error(error);
-        }
-    }
-
 // Use useEffect hook to call fetchData when component mounts
-    useEffect(() => {
-        setTimeout(()=>{fetchData(endPoint)}, 5000);
-    }, [endPoint, loading]);
+    useEffect( () => {
+            // Define an async function to fetch data from API
+            async function fetchData(endPoint) {
+                try {
+                    let headers = new Headers()
+                    headers.append('ngrok-skip-browser-warning', true)
+                    // headers.append('Access-Control-Allow-Origin', '*')
+                    // Make a GET request and get the response
+                    const response = await fetch(endPoint, {headers: headers});
+                    // Convert the response to JSON format
+                    const json = await response.json();
+                    // Update the data state with the JSON data
+                    setData(json);
+                    // Set the loading state to false
+                    setLoading((c)=> c+1);
+                } catch (error) {
+                    // Handle any errors
+                    console.error(error);
+                }
+                setTimeout(()=>{fetchData(endPoint)}, 5000)
+            }
+            fetchData(endPoint);
+        }
+    , [endPoint, loading]);
 
 // Return JSX elements for rendering the table
     return (
